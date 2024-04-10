482cb5663a9753961e3be721bbc46509b4e42838
Thuan Hoang <thuangg456@gmail.com>
2024-04-10 20:23:56 +0700

    test 1

--- frontend/src/config/DefaultURL.js
+++ frontend/src/config/DefaultURL.js
@@ -2,7 +2,7 @@ class DefaultURL {
     // static defaultURL = "http://10.0.19.99:5000";
     // static defaultURL = "http://192.168.0.199:5000";
     // static defaultURL = "http://127.0.0.1:5000";
-    static defaultURL = "https://chat-app-be-lqa2.onrender.com/";
+    static defaultURL = "https://chat-app-be-lqa2.onrender.com";
 
     static getURL() {
         return DefaultURL.defaultURL;
