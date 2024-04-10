757e57125d4bf43d11a84030b791ba4481b658b3
Thuan Hoang <thuangg456@gmail.com>
2024-04-10 19:23:11 +0700

    up local

--- frontend/src/config/DefaultURL.js
+++ frontend/src/config/DefaultURL.js
@@ -1,6 +1,7 @@
 class DefaultURL {
     // static defaultURL = "http://10.0.19.99:5000";
-    static defaultURL = "http://192.168.0.199:5000";
+    // static defaultURL = "http://192.168.0.199:5000";
+    static defaultURL = "http://127.0.0.1:5000";
 
     static getURL() {
         return DefaultURL.defaultURL;
